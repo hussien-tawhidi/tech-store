7edfd9306a2d2650d231f9507e45a464a9088cfc
hussien tawhidi <hussientawhidi710@gmail.com>
2025-01-04 11:11:23 +0330

    update db config function

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,40 +1,16 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGO_URI as string;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 export const dbConnect = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      console.log("Db connected");
-      return mongoose;
-    });
-  }
   try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
+    const { connection } = await mongoose.connect(
+      process.env.MONGO_URI as string
+    );
+    if (connection.readyState === 1) {
+      console.log("MongoDB Connected");
+      return Promise.resolve(true);
+    }
+  } catch (error) {
+    console.error(error, "Error connecting to MongoDB");
+    return Promise.reject(error);
   }
-
-  return cached.conn;
 };
-
-export default dbConnect;
