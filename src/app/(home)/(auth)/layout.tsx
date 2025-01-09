613a2c2e20dcefac60f740b2a77c43bd01bb5dd6
hussien tawhidi <hussientawhidi710@gmail.com>
2025-01-09 11:32:39 +0330

    update

--- src/app/(home)/(auth)/layout.tsx
+++ src/app/(home)/(auth)/layout.tsx
@@ -1,14 +1,6 @@
-"use client";
-
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
 
 const UserLayout = ({ children }: { children: React.ReactNode }) => {
-  const { data: session } = useSession();
-
-  const url = process.env.NEXT_PUBLIC_BASE_URL;
-
-  if (!session) redirect(`${url}/user-login`);
+ 
 
   return <>{children}</>;
 };
